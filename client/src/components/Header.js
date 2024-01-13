ef288c3dc4b34a33fa138f0d5eb6948e39c21199
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2024-01-13 12:25:05 +0100

    setuo

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <div>
+            <div>
+                <div className="header1">
+
+                </div>
+            </div>
+        </div>
+    )
+}
