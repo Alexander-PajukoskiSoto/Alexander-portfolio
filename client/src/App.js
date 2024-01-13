3956b12f2d9e939a892f9bde1e45100b7229550c
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2024-01-13 20:20:21 +0100

    import logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 import Aside from './components/Aside';
 function App() {
   return (
     <div className="App">
+      <Header />
       <Aside />
     </div>
   );
